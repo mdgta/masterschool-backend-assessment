df3a56fc4de299b678c9cacd3953ddd7f33be10a
MD <wkinfo@walla.com>
2023-02-17 12:33:17 +0200

    Part I - forgot to use express.json()

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ dotenv.config();
 // create server
 const app = express();
 
+// accept request body data
+app.use(express.json());
+
 // routes
 app.get("/", (req, res) => {
 	res.status(200).json({message: "Welcome to the Unsplash API!"});
