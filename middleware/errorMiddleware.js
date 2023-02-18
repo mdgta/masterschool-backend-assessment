c48ee813f3091dc8cfe14744e18c6938693f2bf5
MD <wkinfo@walla.com>
2023-02-18 17:41:50 +0200

    small fix for errorMiddleware

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -3,7 +3,7 @@ export const errorHandler = (err, req, res, next) => {
 		error: err.message
 	};
 	if (process.env.NODE_ENV === "development") {
-		responseObject.trace = err.trace;
+		responseObject.stack = err.stack;
 	}
 	const code = res.statusCode !== 200 ? res.statusCode : 500;
 	res.status(code).json(responseObject);
